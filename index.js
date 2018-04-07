37c7bc5ef832e95880eda21e8f226082f704a465
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-07 18:45:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    startBlock = parseInt(startBlock)
+    endBlock = parseInt(endBlock)
+    let range = endBlock - startBlock
+  
+    if (range <= blockRange){
+      return `within range by ${range}`
+    } else {
+      return `${range} blocks out of range`
+    }
+  }
+}
